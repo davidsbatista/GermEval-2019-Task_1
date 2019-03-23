97abe1904025738108c2f62d8bd5656e9486a79a
David Soares Batista <david.batista@comtravo.com>
2019-03-23 02:05:52 +0100

    bug fix in parsing and first usefull results

--- parse.py
+++ parse.py
@@ -1,118 +1,118 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import functools
-import sys
-import random
-
-# import spacy
-import time
+from collections import defaultdict
+from os.path import join
 
 from bs4 import BeautifulSoup, Tag
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 
-
-def load_data():
-    print("Pre-processing data")
-    soup = BeautifulSoup(open(sys.argv[1]), "html.parser")
-    data_x = []
-    data_y = []
-    for book in soup.findAll("book"):
-        x = {'title': book.title.text,
-             'body': book.body.text,
-             'authors': book.authors.text,
-             'published': book.published.text,
-             'isbn': book.isbn.text}
-
-        y = [t.text for categ in book.categories for t in categ if isinstance(t, Tag)]
-        data_x.append(x)
-        data_y.append(y)
-
-    print(f'Processed {len(data_x)} documents')
-
-    return data_x, data_y
+from nltk.corpus import stopwords
 
 
-def split_data(data_x, data_y):
-    # split data, have a hold-out set of 20%
-    n_samples = int(len(data_x) * 0.2)
-    hold_out_idx = random.sample(range(0, len(data_x)), n_samples)
-    hold_out_x = []
-    hold_out_y = []
-    train_x = []
-    train_y = []
-    for idx, (x, y) in enumerate(zip(data_x, data_y)):
-        if idx in hold_out_idx:
-            hold_out_x.append(x)
-            hold_out_y.append(y)
-        else:
-            train_x.append(x)
-            train_y.append(y)
-    print("all data: ", len(data_x))
-    print("train   : ", len(train_x))
-    print("holdout : ", len(hold_out_x))
+def load_data(file):
+    base_path = 'blurbs_dev_participants/'
+    full_path = join(base_path, file)
 
-    return train_x, train_y, hold_out_x, hold_out_y
+    topics_distribution = defaultdict(int)
 
+    with open(full_path, 'rt') as f_in:
+        print("Loading {}".format(full_path))
+        soup = BeautifulSoup(f_in, "html.parser")
+        data_x = []
+        data_y = []
+        for book in soup.findAll("book"):
+            x = {'title': book.title.text,
+                 'body': book.body.text,
+                 'authors': book.authors.text,
+                 'published': book.published.text,
+                 'isbn': book.isbn.text}
 
-def tokenize(train_x):
-    nlp = spacy.load('de_core_news_sm')
-    all_docs = []
-    c = 0
-    print("\nBuild docs. for TF-IDF weighted vectors")
-    for x in train_x:
-        tokens = []
-        doc = nlp(x['title'])
-        for token in doc:
-            tokens.append(token)
-        doc = nlp(x['body'])
-        for token in doc:
-            tokens.append(token)
-        all_docs.append(tokens)
-        c += 1
-        if c % 1000 == 0:
-            print(c)
+            if 'train' in full_path:
+                # y = [t.text for categ in book.categories for t in categ if isinstance(t, Tag)]
+                topics = set()
+                for categ in book.categories:
+                    for t in categ:
+                        if isinstance(t, Tag):
+                            if t['d'] == "0":
+                                topics.add(t.text)
+                                topics_distribution[t.text] += 1
+                data_y.append(list(topics))
 
-    print(len(all_docs))
+            data_x.append(x)
 
-    return all_docs
+        print(f'Loaded {len(data_x)} documents')
+
+    return data_x, data_y, topics_distribution
 
 
 def main():
 
-    # load a pre-process
-    data_x, data_y = load_data()
+    """
+    Subtasks
+    This shared task consists of two subtask, described below. You can participate in one of
+    them or both.
 
-    from sklearn.preprocessing import MultiLabelBinarizer
+    Subtask A
+    The task is to classify german books into one or multiple most general writing genres (d=0).
+    Therfore, it can be considered a multi-label classification task. In total, there are 8 classes
+    that can be assigned to a book:
+    - Literatur & Unterhaltung,
+    - Ratgeber,
+    - Kinderbuch & Jugendbuch,
+    - Sachbuch,
+    - Ganzheitliches Bewusstsein,
+    - Glaube & Ethik,
+    - Künste,
+    - Architektur & Garten.
+
+
+    Subtask B
+    The second task is a hierarchical multi-label classification into multiple writing genres.
+    In addition to the very general writing genres additional genres of different specificity can
+    be assigned to a book. In total, there are 343 different classes that are hierarchically
+    structured.
+
+    :return:
+    """
+
+    # load train data
+    train_data_x, train_data_y, topics_distribution = load_data('blurbs_train.txt')
+
+    for k, v in topics_distribution.items():
+        print(k, v)
+
+    # load dev data
+    # dev_data_x, dev_data_y = load_data('blurbs_dev_participants.txt')
+
+    # one-hot vectors on y labels
     ml_binarizer = MultiLabelBinarizer()
-    y_labels = ml_binarizer.fit_transform(data_y)
+    y_labels = ml_binarizer.fit_transform(train_data_y)
+    print('Total of {} classes'.format(len(ml_binarizer.classes_)))
 
-    print('Total of {len(ml_binarizer.classes_} classes')
+    train_data_x = train_data_x[:1000]
+    data_y = y_labels[:1000]
 
-    data_x = data_x[:100]
-    data_y = y_labels[:100]
-    new_data_x = [x['title'] + "SEP" + x['body'] for x in data_x]
+    new_data_x = [x['title'] + "SEP" + x['body'] for x in train_data_x]
 
     # split into train and hold out set
-    # train_x, train_y, hold_out_x, hold_out_y = split_data(new_data_x, data_y)
-    train_x, test_x, train_y, test_y = train_test_split(new_data_x, data_y, test_size=0.20)
+    train_x, test_x, train_y, test_y = train_test_split(new_data_x,
+                                                        data_y,
+                                                        random_state=42,
+                                                        test_size=0.20)
 
-    # ToDo: set a baseline
-    # build TF-IDF weighted vectors
-    # all_docs = tokenize(train_x)
-
-    # apply logistic regression with multi-label
-
-    # TfidfVectorizer(stop_words=stop_words))
+    # set a baseline: build TF-IDF weighted vectors and apply
+    # logistic regression with multi-label
+    stop_words = set(stopwords.words('german'))
     pipeline = Pipeline([
-        ('tfidf', TfidfVectorizer()),
-        ('clf', OneVsRestClassifier(LogisticRegression(solver='sag'), n_jobs=1))
+        ('tfidf', TfidfVectorizer(stop_words=stop_words)),
+        ('clf', OneVsRestClassifier(LogisticRegression(solver='sag', max_iter=1000), n_jobs=2))
     ])
     parameters = {
         'tfidf__max_df': (0.25, 0.5, 0.75),
@@ -121,9 +121,9 @@ def main():
         "clf__estimator__class_weight": ['balanced', None],
     }
 
-    grid_search_tune = GridSearchCV(pipeline, parameters, cv=2, n_jobs=2, verbose=10)
-    grid_search_tune.fit(train_x, train_y)
+    grid_search_tune = GridSearchCV(pipeline, parameters, cv=2, n_jobs=2, verbose=2)
 
+    grid_search_tune.fit(train_x, train_y)
     print("Best parameters set:")
     print(grid_search_tune.best_estimator_.steps)
 
