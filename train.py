a9a6c80f8b29d533cd64c7549986009fd846a8b4
David Soares Batista <david.batista@comtravo.com>
2019-04-03 15:42:01 +0200

    cleaning

--- train.py
+++ train.py
@@ -130,7 +130,7 @@ def train_baseline(train_data_x, train_data_y):
     predictions = best_clf.predict(test_x)
     report = classification_report(test_y, predictions, target_names=ml_binarizer.classes_)
     print(report)
-    with open('models_subtask_a_report.txt', 'wt') as f_out:
+    with open('results/models_subtask_a_report.txt', 'wt') as f_out:
         f_out.write(report)
 
     # train a classifier on all data using the parameters that yielded best result
@@ -170,7 +170,7 @@ def train_random_forest(train_x, train_y, test_x, test_y, ml_binarizer, level=No
     predictions = best_clf.predict(test_x)
     report = classification_report(test_y, predictions, target_names=ml_binarizer.classes_)
     print(report)
-    with open('models_subtask_b_report_{}.txt'.format(level), 'wt') as f_out:
+    with open('results/models_subtask_b_report_{}.txt'.format(level), 'wt') as f_out:
         f_out.write(report)
 
     # train a classifier on all data using the parameters that yielded best result
@@ -309,10 +309,10 @@ def subtask_b(train_data_x, train_data_y, dev_data_x):
     # sub-task B Train 3 classifiers, one for each level, random forests
     classifiers, ml_binarizers = train_random_forests_multilabel(train_data_x, train_data_y)
 
-    with open('models_3_labels.pkl', 'wb') as f_out:
+    with open('results/models_3_labels.pkl', 'wb') as f_out:
         pickle.dump(classifiers, f_out)
 
-    with open('ml_binarizers_3_labels.pkl', 'wb') as f_out:
+    with open('results/ml_binarizers_3_labels.pkl', 'wb') as f_out:
         pickle.dump(ml_binarizers, f_out)
 
     # apply on dev data
@@ -355,86 +355,6 @@ def train_cnn_sent_class(train_data_x, train_data_y):
     pass
 
 
-def data_analysis(train_data_x, train_data_y, labels):
-    # TODO: top-words per class
-
-    author_topic = defaultdict(int)
-
-    # for sample_x, sample_y in zip(train_data_x, train_data_y):
-    #     # new_data_x = [x['title'] + " SEP " + x['body'] for x in train_data_x]
-    #     print(len(sample_x['title'].split()))
-    #     print(len(sample_x['body'].split()))
-    #     print(len(sample_y))
-    #     print()
-
-    # for sample_x, sample_y in zip(train_data_x, train_data_y):
-    #     # new_data_x = [x['title'] + " SEP " + x['body'] for x in train_data_x]
-    #     print(sample_x['authors'].split(","))
-    #     print(sample_y)
-    #     print()
-    #
-    #     for x in sample_x['authors'].split(","):
-    #         author_topic[x.strip()] += 1
-    #
-    # for k, v in author_topic.items():
-    #     if v > 1:
-    #         print(k, v)
-
-    # extract hierachichal structure
-    level_0 = ['Architektur & Garten',
-               'Ganzheitliches Bewusstsein',
-               'Glaube & Ethik',
-               'Kinderbuch & Jugendbuch',
-               'Künste',
-               'Literatur & Unterhaltung',
-               'Ratgeber',
-               'Sachbuch']
-
-    hierarchical_level_0 = defaultdict(list)
-    hierarchical_level_1 = defaultdict(list)
-    all_lines = []
-
-    with open('blurbs_dev_participants/hierarchy.txt', 'rt') as f_in:
-        for line in f_in:
-            all_lines.append(line)
-            parts = line.split('\t')
-            if any(x == parts[0].strip() for x in level_0):
-                hierarchical_level_1[parts[1].strip()] = []
-                hierarchical_level_0[parts[0].strip()].append(parts[1].strip())
-
-    level_1 = list(hierarchical_level_1.keys())
-    for line in all_lines:
-        parts = line.split('\t')
-        if any(x == parts[0].strip() for x in level_1):
-            hierarchical_level_1[parts[0].strip()].append(parts[1].strip())
-
-    print("\nLevel 0")
-    for k, v in hierarchical_level_0.items():
-        print(k, '\t', len(v))
-
-    print("\nLevel 1")
-    for k, v in hierarchical_level_1.items():
-        print(k, '\t', len(v))
-
-    """
-    from pandas import DataFrame
-    df_stats_level_0 = DataFrame.from_dict(labels['0'], orient='index', columns=['counts'])
-
-    print(df_stats_level_0)
-    df_stats_level_0.plot(y='counts', kind='bar', legend=False, grid=True, figsize=(15, 8))
-    print()
-
-    df_stats_level_1 = DataFrame.from_dict(labels['1'], orient='index', columns=['counts'])
-    print(df_stats_level_1)
-    df_stats_level_1.plot(y='counts', kind='bar', legend=False, grid=True, figsize=(15, 8))
-    print()
-
-    df_stats_level_2 = DataFrame.from_dict(labels['2'], orient='index', columns=['counts'])
-    print(df_stats_level_2)
-    df_stats_level_2.plot(y='counts', kind='bar', legend=False, grid=True, figsize=(15, 8))
-    """
-
-
 def main():
     # ToDo: produce a run for subtask-B
     # ToDo: explore the hierarchical structure and enforce it in the classifiers
@@ -444,10 +364,6 @@ def main():
 
     # load dev data
     dev_data_x, _, _ = load_data('blurbs_dev_participants.txt')
-
-    # do some data analysis
-    #data_analysis(train_data_x, train_data_y, labels)
-    #exit(-1)
 
     # train subtask_a
     data_y_level_0 = []
