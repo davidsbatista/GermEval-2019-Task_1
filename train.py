c56d69abf45febd51044cdff6d6daaed285e65d0
David Soares Batista <david.batista@comtravo.com>
2019-04-06 20:49:38 +0200

    WIP

--- train.py
+++ train.py
@@ -158,7 +158,7 @@ def train_baseline(train_data_x, train_data_y):
     for pred, true, text in zip(predictions, test_y, test_x):
         all_zeros = not np.any(pred)
         if all_zeros:
-            print(true)
+            print(pred)
             print(ml_binarizer.inverse_transform(true))
             print(text)
             print()
