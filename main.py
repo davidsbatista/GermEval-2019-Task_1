7730341a2fc7b605aa0fb54c1cbf7f29a017b414
David Soares Batista <david.batista@comtravo.com>
2019-04-08 23:06:49 +0200

    WIP

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def main():
                 training_labels=training_labels,
                 validation_inputs=[],
                 validation_labels=[],
-                epochs=10,
+                epochs=100,
                 batch_size=config["training"]["batch_size"],
                 checkpoint_every=config["training"]["checkpoint_every"])
 
