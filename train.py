7dd2aace43917880e58524f035afdc5703342694
David Soares Batista <david.batista@comtravo.com>
2019-04-14 22:11:05 +0200

    WIP

--- train.py
+++ train.py
@@ -829,7 +829,7 @@ def subtask_b(train_data_x, train_data_y, dev_data_x, clf='tree'):
                 pred_bin = (predictions > filter).astype(int)[0]
                 indexes = pred_bin.nonzero()
                 for x in indexes:
-                    label = binarizer.classes_[x]
+                    label = binarizer.classes_[int(x)]
                     print("label: ", label, type(label))
                     print("label: ", str(label), type(str(label)))
                     print(classification[data['isbn']][1])
