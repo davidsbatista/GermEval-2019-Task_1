7167795045eae5827ddfe1a0dcf0dc57b3e3a6bf
David Soares Batista <david.batista@comtravo.com>
2019-06-04 18:07:06 +0200

    WIP

--- generate_dev_gold_file.py
+++ generate_dev_gold_file.py
@@ -11,37 +11,20 @@ def main():
     train_data_all_x, train_data_all_y, labels = load_data('blurbs_train_all.txt', dev=False)
 
     c = 0
-    for x, y in zip(train_data_all_x, train_data_all_y):
-        if x['isbn'] in [x['isbn'] for x in dev_gold]:
-            print(x['isbn'])
-            print(y)
-            print()
-            c += 1
-
-    print(c)
-    print()
-
-    """
-    with open('answer.txt', 'wt') as f_out:
+    with open('gold.txt', 'wt') as f_out:
         f_out.write(str('subtask_a\n'))
-        for x in dev_data_x:
-            isbn = x['isbn']
-            f_out.write(str(isbn) + '\t' + '\t'.join(classification[isbn][0]) + '\n')
+        for x, y in zip(train_data_all_x, train_data_all_y):
+            all_labels = set([label[0] for label in y])
+            if x['isbn'] in [x['isbn'] for x in dev_gold]:
+                f_out.write(x['isbn'] + '\t' + '\t'.join(all_labels) + '\n')
 
         f_out.write(str('subtask_b\n'))
-        for x in dev_data_x:
-            isbn = x['isbn']
-            output = isbn + '\t'
-            output += '\t'.join(classification[isbn][0])
-            if len(classification[isbn][1]) > 0:
-                output += '\t'
-                output += '\t'.join(classification[isbn][1])
-                if len(classification[isbn][2]) > 0:
-                    output += '\t'
-                    output += '\t'.join(classification[isbn][2])
-            output += '\n'
-            f_out.write(output)
-    """
+        for x, y in zip(train_data_all_x, train_data_all_y):
+            all_labels = set([x for label in y for x in label.values()])
+            if x['isbn'] in [x['isbn'] for x in dev_gold]:
+                f_out.write(x['isbn'] + '\t' + '\t'.join(all_labels) + '\n')
+
+        c += 1
 
 
 if __name__ == '__main__':
