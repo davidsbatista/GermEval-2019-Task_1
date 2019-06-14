a6c2736b6dd293591a4e88e27c00e0d5e5ca68bb
David Soares Batista <david.batista@comtravo.com>
2019-06-14 11:41:48 +0200

    WIP

--- train.py
+++ train.py
@@ -95,7 +95,7 @@ def train_logit_tf_idf(train_data_x, train_data_y, level_label):
         'tfidf__min_df': [0.1, 0.2],
         "clf__estimator__C": [0.01, 0.1, 1, 10, 50, 100, 300],
     }
-    grid_search_tune = GridSearchCV(pipeline, parameters, cv=3, n_jobs=3, verbose=3)
+    grid_search_tune = GridSearchCV(pipeline, parameters, cv=3, n_jobs=3, verbose=10)
     grid_search_tune.fit(train_x, train_y,)
 
     # measuring performance on test set
