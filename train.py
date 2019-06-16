7c7c3cec99005dde7ebe137529d5bb6dd295f7dd
David Soares Batista <david.batista@comtravo.com>
2019-06-16 22:03:10 +0200

    WIP

--- train.py
+++ train.py
@@ -291,21 +291,25 @@ def train_logit_tf_idf(train_data_x, train_data_y, level_label):
                                                         test_size=0.30)
 
     pipeline = Pipeline([
-        ('tfidf', TfidfVectorizer(stop_words=None, ngram_range=(1, 2), lowercase=False,
-                                  analyzer='word', tokenizer=dummy_fun, preprocessor=dummy_fun)),
-        ('clf', OneVsRestClassifier(LogisticRegression(class_weight='balanced',
-                                                       solver='sag',
-                                                       max_iter=5000), n_jobs=3))
+        ('tfidf', TfidfVectorizer(stop_words=None,
+                                  ngram_range=(1, 2), max_df=0.75,
+                                  analyzer='word',
+                                  tokenizer=dummy_fun,
+                                  preprocessor=dummy_fun,
+                                  )),
+        ('clf', OneVsRestClassifier(
+            LogisticRegression(class_weight='balanced', solver='sag', max_iter=50000),
+            n_jobs=3))
     ])
 
+    # stop_words=stop_words, ngram_range=(1, 2), max_df=0.75)
+
     parameters = {
-        # 'tfidf__lowercase': (True, False),
-        # 'tfidf__norm': ['l1', 'l2'],
-        # 'tfidf__max_df': [0.8, 0.9],
-        # "clf__estimator__C": [1, 10, 50, 100, 300],
-        # 'tfidf__lowercase': (True, False),
         "clf__estimator__C": [300],
+        'tfidf__lowercase': (True, False),
+        'tfidf__norm': ('l1', 'l2'),
     }
+
     grid_search_tune = GridSearchCV(pipeline, parameters, cv=3, n_jobs=10, verbose=2)
     grid_search_tune.fit(train_x, train_y,)
 
