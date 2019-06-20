0c0aa0d27c96ffefc9bc447ce949a8e1ee485cea
David Soares Batista <david.batista@comtravo.com>
2019-06-20 19:07:45 +0200

    WIP

--- initialization_weights.py
+++ initialization_weights.py
@@ -173,26 +173,35 @@ def main():
     dev_vector = vectorize_dev_data(dev_data_x, max_sent_len, token2idx)
     predictions = model.predict(dev_vector, verbose=1)
 
+    # tune threshold for different levels?
+    # 0-7 top-level
+    # 8-X 1st_level
+    # X-343 2nd_level
+
     filtered = np.array(len(labels2idx) * [0.3])
     pred_bin = (predictions > filtered).astype(int)
     idx2labels = {v: k for k, v in labels2idx.items()}
 
-    # subtask-a
-    for row_pred, sample in zip(pred_bin, dev_data_x):
-        print(sample['isbn'], end='\t')
-        if np.count_nonzero(row_pred) > 0:
-            for x in np.nditer(np.nonzero(row_pred)):
-                if int(x) <= 7:
-                    print(idx2labels[int(x)], end='\t')
-        print()
+    with open('answer.txt', 'wt') as f_out:
 
-    # subtask-b
-    for row_pred, sample in zip(pred_bin, dev_data_x):
-        print(sample['isbn'], end='\t')
-        if np.count_nonzero(row_pred) > 0:
-            for x in np.nditer(np.nonzero(row_pred)):
-                print(idx2labels[int(x)], end='\t')
-        print()
+        # subtask-a
+        f_out.write(str('subtask_a\n'))
+        for row_pred, sample in zip(pred_bin, dev_data_x):
+            print(sample['isbn'], end='\t')
+            if np.count_nonzero(row_pred) > 0:
+                for x in np.nditer(np.nonzero(row_pred)):
+                    if int(x) <= 7:
+                        print(idx2labels[int(x)], end='\t')
+            print()
+
+        # subtask-b
+        f_out.write(str('subtask_b\n'))
+        for row_pred, sample in zip(pred_bin, dev_data_x):
+            print(sample['isbn'], end='\t')
+            if np.count_nonzero(row_pred) > 0:
+                for x in np.nditer(np.nonzero(row_pred)):
+                    print(idx2labels[int(x)], end='\t')
+            print()
 
 
 if __name__ == '__main__':
