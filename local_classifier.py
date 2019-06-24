76cc5bfa8ef00d389b0aa78c771424f7cfe4b2a7
David Soares Batista <david.batista@comtravo.com>
2019-06-24 18:47:51 +0200

    WIP

--- local_classifier.py
+++ local_classifier.py
@@ -98,7 +98,7 @@ def train_bi_lstm(train_data_x, train_data_y):
 
 
 def train_han(train_data_x, train_data_y):
-    token2idx, max_sent_len = build_token_index(train_data_x)
+    token2idx, max_sent_len, _ = build_token_index(train_data_x)
 
     # y_data: encode into one-hot vectors
     ml_binarizer = MultiLabelBinarizer()
