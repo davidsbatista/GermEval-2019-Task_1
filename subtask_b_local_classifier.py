47420148cbd4d09e9bfd8a3e78dc3de330384ff7
David Soares Batista <david.batista@comtravo.com>
2019-07-27 23:09:54 +0200

    WIP

--- subtask_b_local_classifier.py
+++ subtask_b_local_classifier.py
@@ -324,7 +324,7 @@ def main():
     dev_data_x, _, _ = load_data('blurbs_dev_participants.txt', dev=True)
 
     # train subtask_b
-    subtask_b(train_data_x, train_data_y, dev_data_x, True)
+    subtask_b(train_data_x, train_data_y, dev_data_x, False)
 
     # load submission/test data
     # train_data_x, train_data_y, labels = load_data('blurbs_train_all.txt', dev=False)
