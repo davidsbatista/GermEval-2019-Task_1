d19024e935c24e831c875eb9352e788a901cc9d8
David Soares Batista <david.batista@comtravo.com>
2019-07-28 13:26:39 +0200

    WIP

--- subtask_b_global_classifier.py
+++ subtask_b_global_classifier.py
@@ -113,8 +113,8 @@ def init_f(shape, dtype=None):
 def build_neural_network(weight_matrix, input_size, token2idx):
     # alphabet_size = vocab_size
     embedding_size = 300
-    conv_layers = [[256, 5], [256, 3], [256, 2], [256, 1]]
-    fully_connected_layers = [2048, 2048]
+    conv_layers = [[256, 7], [256, 5], [256, 3], [256, 2], [256, 1]]
+    fully_connected_layers = [1024, 1024]
     dropout_p = 0.5
     num_of_classes = weight_matrix.shape[1]
     loss = "binary_crossentropy"
